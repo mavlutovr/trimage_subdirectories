cc1195352ee302d64871476540e3186e3f99ac82
roma <mavlutovr@ya.ru>
2020-09-07 22:32:11 +0300

    Config

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # trimage_subdirectories
+
+I made the script for compress millions images of my project https://tuteta.ru/
+
+You can use it for compress many images a long time. After restart the script continue compress images from last compressed image.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+// Config
+const config = require('config');
 
+
+// DB
 // https://www.npmjs.com/package/smoothstore
 const { Datastore } = require('smoothstore');
 const data = new Datastore("TrimageSubdirectories");
 
 
+
+
 let lastI = data.get('i') || 0;
 
 console.log('lastI', lastI);
+console.log('directoryAbsolutePath', config.directoryAbsolutePath);
 lastI ++;
 data.set('i', lastI);
