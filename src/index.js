405ac33afb94089b6bbb17f1b3c6163e73dfcd08
roma <mavlutovr@ya.ru>
2020-11-26 10:24:45 +0300

    fix: skip diles with no access mod

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ const getNextFilePath = (lastFilePath, prevFileName) => {
 
       if (newElement) {
         try {
-          if (!/(\.png|\.jpg)/.test(newElement))  throw new Error('Strange file name');
+          if (!/(\.png|\.jpg)$/.test(newElement))  throw new Error('Strange file name');
           fs.statSync(lastFilePath + '/' + newElement);
           ok = true;
         }
